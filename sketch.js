666f0682f82af4f6320e8944c0afff2c0797873a
Angelino <angelino_m@outlook.com>
2018-01-11 00:11:24 +0100

    vector for mouse position

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 var pressed = false;
+var mousev;
+var offset;
 
 function setup() { // RUNS ONCE AT THE BEGGINING!
     createCanvas(400, 400);
-
+    mousev = createVector(mouseX, mouseY);
+    offset = 3;
 }
 
 
 function draw() { // FOREVER LOOP!
+    mousev.x = mouseX;
+    mousev.y = mouseY;
+
     background(51);
     noStroke();
     if(pressed){
-        ellipse(mouseX,mouseY,25);
+        ellipse(mousev.x, mousev.y, 40);
     }
     else{
-        ellipse(mouseX,mouseY,5);
+        ellipse(mousev.x, mousev.y, 20);
     }
 
 }
@@ -23,6 +29,6 @@ function mousePressed() { // EVENT!
 
 }
 
-function mouseReleased() {
+function mouseReleased() { // EVENT!
     pressed = false;
 }
