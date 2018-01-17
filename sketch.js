755410e0f1e26a79b7d02fd0ee0992c643f8f434
Angelino <angelino_m@outlook.com>
2018-01-17 15:23:35 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,9 @@ var sliderD;
 var sliderN;
 
 function setup() {
-  createCanvas(400, 400);
-  sliderD = createSlider(1, 20, 10, 1);
-  sliderN = createSlider(1, 20, 10, 1);
-  sliderD.input(draw);
-  sliderN.input(draw);
+  createCanvas(500, 500);
+  sliderD = createSlider(1, 20, 10);
+  sliderN = createSlider(1, 20, 10);
 }
 
 function draw() {
@@ -16,27 +14,19 @@ function draw() {
   n = sliderN.value();
   var k = n / d;
   background(51);
-  push();
   translate(width / 2, height / 2);
 
   beginShape();
   stroke(255);
   noFill();
   strokeWeight(1);
-  for (var a = 0; a < TWO_PI * reduceDenominator(n, d); a += 0.02) {
+  for (var a = 0; a < TWO_PI * d; a += 0.02) {
     var r = 200 * cos(k * a);
     var x = r * cos(a);
     var y = r * sin(a);
     vertex(x, y);
   }
+  stroke(0,0,0,0);
   endShape(CLOSE);
-  pop();
-  noLoop();
-}
-
-function reduceDenominator(numerator, denominator) {
-    function rec(a, b) {
-        return b ? rec(b, a % b) : a;
-    }
-    return denominator / rec(numerator, denominator);
+  
 }
